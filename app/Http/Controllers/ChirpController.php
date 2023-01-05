a6cc641c5e0ac3a09b9c8c391e633feb017236f0
Shogo Arai <noonarai@gmail.com>
2023-01-05 17:19:39 +0900

    sail php artisan make:model -a Chirp

---
+++ app/Http/Controllers/ChirpController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreChirpRequest;
+use App\Http\Requests\UpdateChirpRequest;
+use App\Models\Chirp;
+
+class ChirpController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreChirpRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreChirpRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Chirp  $chirp
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Chirp $chirp)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Chirp  $chirp
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Chirp $chirp)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateChirpRequest  $request
+     * @param  \App\Models\Chirp  $chirp
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateChirpRequest $request, Chirp $chirp)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Chirp  $chirp
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Chirp $chirp)
+    {
+        //
+    }
+}
